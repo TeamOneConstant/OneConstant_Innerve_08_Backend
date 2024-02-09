e8097cb4e1da8dc2d51e3654c38396d79a241125
Kolwankar-Siddhiraj <siddhirajk77@gmail.com>
2024-02-09 12:30:26 +0530

    updated accounts/admin.py

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from accounts.models import *
 
 # Register your models here.
+
+
+class UserAdminConfig(UserAdmin):
+    model = CustomUser
+    search_fields = ('full_name', 'mobile_number')
+    list_filter = ('full_name', 'mobile_number')
+    ordering = ('-id',)
+    list_display = ('mobile_number',)
+    fieldsets = (
+        ("Details", {'fields': ('user_type', 'full_name','mobile_number', 
+                                'status','is_verified')}),
+        ('Permissions', {'fields': ('is_staff', 'user_permissions')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('user_type', 'full_name', 'mobile_number', 'is_staff')}
+         ),
+    )
+
+
+admin.site.register(CustomUser, UserAdminConfig)
+
+
