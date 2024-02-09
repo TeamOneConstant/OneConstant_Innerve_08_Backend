b57d2e74c7ce54fd756ea511912fffc9c92aa10a
Kolwankar-Siddhiraj <siddhirajk77@gmail.com>
2024-02-09 12:17:46 +0530

    fixed deployment issue

--- backend/urls.py
+++ backend/urls.py
@@ -15,8 +15,20 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from backend import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # apps
+    path('api/v1/accounts', include('accounts.urls'))
 ]
+
+
+# add at the last
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
